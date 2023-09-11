cc1bf16098f80ab8b702efc7bbe8c167de38348b
Sam Elliott <selliott452@gmail.com>
2023-09-11 07:56:08 -0400

    #9 - add rest controller to delete all

--- src/main/kotlin/com/elliott/dicethrone/api/OppenheimerController.kt
+++ src/main/kotlin/com/elliott/dicethrone/api/OppenheimerController.kt
@@ -5,7 +5,9 @@ import com.elliott.dicethrone.domain.game.GameRepository
 import com.elliott.dicethrone.domain.player.PlayerRepository
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping("/v1")
 class OppenheimerController(
         private val playerRepository: PlayerRepository,
